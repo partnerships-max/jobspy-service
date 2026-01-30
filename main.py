f23d553e380bb56c6cc779dd35162fdd6cd543c1
partnerships-max <partnerships@riseopp.com>
2026-01-30 16:15:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ jobs = scrape_jobs(
     site_name=["google", "indeed", "linkedin"],
     search_term="SEO",
     location="United States",
-    hours_old=168,
+    hours_old=24,
     fetch_full_description=False
 )
 
